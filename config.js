bfad7c95b2ed8da38b9d6217f2a634cd62dc0218
Diixinh <88525758+Diixinh@users.noreply.github.com>
2021-12-24 01:39:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Nay là 24/12/2021, một ngày Giáng Sinh",
-    introTitle: 'Chào Thành đáng iu',
+    titleWeb: ".....",
+    introTitle: 'Quỳnh ơi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Mình biết là bạn ngại yêu, nhưng mà hãy thử 1 lần đi 🥰',
-    desc: 'Tuy mình không hoàn hảo và ở gần như những người bạn thân cậu, nhưng mà mình sẽ luôn để cậu cảm thấy an toàn ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: 'Bấm vào đây đi',
+    title: 'Ngày mai mua đồ ăn cho Thành nha',
+    desc: 'Trà sữa kem lăn rất cảm ơn bạn Quỳnh ',
+    btnYes: 'Ok mai tao bao',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại tốt với mình thế',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Chúc bạn một mùa Giáng Sinh zui zẻ',
-    btnAccept: 'Yêu và thương cậu <3',
+    messDesc: 'Bạn mang lên lúc 4 5h sáng mình vẫn nhận nha',
+    btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
